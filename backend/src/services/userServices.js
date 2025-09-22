874de803da011265f85dc51fd61c414bed6d27fb
Hasan Alasker <hasanalasker2004@gmail.com>
2025-09-22 09:09:30 +0300

    JWT

--- backend/src/services/userServices.js
+++ backend/src/services/userServices.js
@@ -1,5 +1,6 @@
 import usersModel from "../models/usersModel.js";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async ({
   fullName,
@@ -11,28 +12,43 @@ export const register = async ({
   const findUser = await usersModel.findOne({ email });
 
   if (findUser) {
-    return { data: "User already exists", statusCode:400 };
+    return { data: "User already exists", statusCode: 400 };
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10)
-  const newUser = new usersModel({ fullName, email, password:hashedPassword, phone, address });
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new usersModel({
+    fullName,
+    email,
+    password: hashedPassword,
+    phone,
+    address,
+  });
   await newUser.save();
 
-  return {data: newUser, statusCode:200};
+  return { data: generateJWT({ fullName, email }), statusCode: 200 };
 };
 
 export const login = async ({ email, password }) => {
   const findUser = await usersModel.findOne({ email });
 
   if (!findUser) {
-    return { data:"User doesn't exist", statusCode:200 };
+    return { data: "User doesn't exist", statusCode: 200 };
   }
 
   const passwordMatch = await bcrypt.compare(password, findUser.password);
 
   if (!passwordMatch) {
-    return { data: "Incorrect email or password" , statusCode:400 };
+    return { data: "Incorrect email or password", statusCode: 400 };
   }
 
-  return {data: findUser, statusCode:200};
+  return {
+    data: generateJWT({ fullName: findUser.fullName, email }),
+    statusCode: 200,
+  };
+};
+
+const generateJWT = (data) => {
+  return jwt.sign(data, "cyEZWvpsgpGNMtBKWxKqAkG57UIY482k", {
+    expiresIn: "24h",
+  });
 };
