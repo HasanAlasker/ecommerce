889b2f5b3f05052217b52b884bc5f30663341e08
Hasan Alasker <hasanalasker2004@gmail.com>
2025-09-30 14:52:38 +0300

    clear cart added

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import { useCart } from "../context/CartContext";
 
 export default function Cart() {
   const { token } = useAuth();
-  const { cart, loading } = useCart()
+  const { cart, loading, clearCart } = useCart();
 
   if (!token) {
     return <h1 className="xLarge center alone">Please login first!</h1>;
@@ -17,30 +17,45 @@ export default function Cart() {
 
   return (
     <div>
-      <h1 className="xLarge center">My Cart</h1>
-      
+      <div className="check-container">
+        <h1 className="xLarge ">My Cart</h1>
+      </div>
+
       {cart && cart.items && cart.items.length > 0 ? (
         <>
-          <div className="cart-summary center">
-            <p className="mid gray">Total Items: {cart.totalItems}</p>
-            <p className="large priColor bold">Total: {cart.totalAmount} JD</p>
+          <div className="check-container">
+            <p className="mid gray" style={{marginTop:'1rem'}}>Total Items: {cart.totalItems}</p>
           </div>
-          
+
           <div className="card-cont">
             {cart.items.map((item) => (
-              <Card 
-                key={item._id} 
+              <Card
+                key={item._id}
                 id={item.productId?._id || item.productId}
                 name={item.productId?.name || item.name}
                 image={item.productId?.image || item.image}
                 price={item.productId?.price || item.price}
-                discountedPrice={item.productId?.discountedPrice || item.discountedPrice}
+                discountedPrice={
+                  item.productId?.discountedPrice || item.discountedPrice
+                }
                 stock={item.productId?.stock || item.stock}
                 quantity={item.quantity}
                 cartPage={true}
                 isAdmin={false}
               />
             ))}
+          </div>
+          <div className="check-container marginBo">
+            <div className="cart-checkout-box ">
+              <p className="mid gray">Total Items: {cart.totalItems}</p>
+              <p className="large priColor bold">
+                Total: {cart.totalAmount} JD
+              </p>
+            </div>
+            <div className="ctaCard">
+              <button className="square mid">Confirm order</button>
+              <button className="square secSq mid" onClick={clearCart}>Clear cart</button>
+            </div>
           </div>
         </>
       ) : (
